daaa98cae07544248dcb38e93039db74fbf1bcd9
Sejal0201 <jainsejal0204@gmail.com>
2026-07-10 00:14:45 +0530

    Update CORS for production

--- knowa-backend/server.js
+++ knowa-backend/server.js
@@ -62,9 +62,19 @@ const quizRoutes = require("./routes/quizRoutes");
 
 const app = express();
 
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL || "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      process.env.CLIENT_URL,
+    ],
     credentials: true,
   })
 );
