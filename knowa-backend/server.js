699cc12248faefcfc30d7abf8ea0e64351e27f57
Sejal0201 <jainsejal0204@gmail.com>
2026-07-10 17:51:24 +0530

    Fix server.js

--- knowa-backend/server.js
+++ knowa-backend/server.js
@@ -69,12 +69,30 @@ const app = express();
 //   })
 // );
 
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       process.env.CLIENT_URL,
+//     ],
+//     credentials: true,
+//   })
+// );
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://knowa-quiz-platform.vercel.app",
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      process.env.CLIENT_URL,
-    ],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
